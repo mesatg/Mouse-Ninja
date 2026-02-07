4e6f7d7d6cee76fcd94bf64087bf6d779ec67dd5
mesatg <gozutokesat2002@gmail.com>
2026-02-08 02:36:22 +0300

    Initial Mouse Ninja project

---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+
+    private Rigidbody targetRb;
+    private GameManager gameManager;
+    private float minSpeed = 12.0f;
+    private float maxSpeed = 16.0f;
+    private float maxTorque = 1.0f;
+    private float xRange = 4.0f;
+    private float ySpawnPos = -4.0f;
+
+    public int pointValue;
+    public ParticleSystem explosionParticle;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        targetRb = GetComponent<Rigidbody>();
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+
+        targetRb.AddForce(RandomForce(), ForceMode.Impulse);
+        targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
+
+        transform.position = RandomSpawnPos();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(minSpeed, maxSpeed);
+    }
+
+    private void OnMouseDown()
+    {
+        if (gameManager.isGameActive)
+        {
+            Destroy(gameObject);
+            Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+            gameManager.UpdateScore(pointValue);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+        if (!gameObject.CompareTag("Bad"))
+        {
+            gameManager.UpdateLives(1);
+            if (gameManager.lives == 0)
+            {
+                gameManager.GameOver();
+            }
+        }
+    }
+
+    float RandomTorque()
+    {
+        return Random.Range(-maxTorque, maxTorque);
+    }
+
+    Vector3 RandomSpawnPos()
+    {
+        return new Vector3(Random.Range(-xRange, xRange), ySpawnPos);
+    }
+}
